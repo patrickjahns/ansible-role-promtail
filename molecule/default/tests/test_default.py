ab8a65f23536a2b0221e0b3599e6000af90190e5
Patrick Jahns <github@patrickjahns.de>
2020-04-11 01:55:20 +0200

    test: refactored test for checking promtail output to not depend on exact version string

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -58,4 +58,5 @@ def test_grpc_socket(host):
 def test_version(host, AnsibleDefaults):
     version = os.getenv('PROMTAIL', AnsibleDefaults['promtail_version'])
     out = host.run("/usr/local/bin/promtail --version").stdout
-    assert "promtail, version " + version in out
+    assert version in out
+    assert "promtail" in out
